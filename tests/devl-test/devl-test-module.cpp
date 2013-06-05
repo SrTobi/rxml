7a85099fc937f93a64a342bdbed32bfc907d9cfd
SirTobi <Code.DataByte@gmail.com>
2013-06-05 14:21:35 +0200

    initial startup

---
+++ tests/devl-test/devl-test-module.cpp
@@ -0,0 +1,9 @@
+//#include "includes.hpp"
+
+/************************************************
+ * 
+ *	This file only sets the test module.
+ *
+ ************************************************/
+#define BOOST_TEST_MODULE  rxml devl test
+#include <boost/test/unit_test.hpp>
---
+++ tests/devl-test/rxml-test/node-test.cpp
@@ -0,0 +1,3 @@
+#include "rxml.hpp"
+#include "test_settings.hpp"
+#include "test_config.hpp"
