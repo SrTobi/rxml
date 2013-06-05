d1d3752ea746ccd97228deb5e62279a05277f303
SirTobi <Code.DataByte@gmail.com>
2013-06-05 20:42:22 +0200

    file for value added.

---
+++ include/rxml/value.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#ifndef _RXML_VALUE_HPP
+#define _RXML_VALUE_HPP
+
+
+#include <rapidxml.hpp>
+#include "get.hpp"
+
+namespace rxml {
+
+
+template<typename _Ch>
+std::basic_string<_Ch> value(const rapidxml::xml_base<_Ch>* entity)
+{
+
+}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+#endif
